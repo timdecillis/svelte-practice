c6004de04d1c72e0d83a1cf1a12f112a1ddfa639
Tim DeCillis <timdecillis@gmail.com>
2024-01-02 18:28:54 -0800

    Added basic button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+  let clicked = false;
+  import Button from "./Button.svelte";
+  const handleClick = () => {
+    clicked = !clicked;
+  }
+</script>
 <h1>Welcome to Svelte</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#if clicked}
+  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{/if}
+
+<Button on:click={handleClick} />
+
---
+++ src/routes/Button.svelte
@@ -0,0 +1,1 @@
+<button on:click >Push me</button>
