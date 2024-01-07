a7350172b552baf30d8b85a97472d3246c1d80e3
Tim DeCillis <timdecillis@gmail.com>
2024-01-06 20:24:58 -0800

    Subscribed to store

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,20 @@
 
   console.log('count:', count)
 
+  let clicks: number;
+
   let selected: string = 'none';
 
   const updateFlavor = (chosen: string) => {
     selected = chosen;
   }
+  const handleClick = () => {
+    count.update((number) => number + 1)
+  }
+  count.subscribe((value) => clicks = value);
 </script>
+
+<button on:click={handleClick} >Number of clicks: {clicks}</button>
 
 <Options updateFlavor={updateFlavor} />
 
