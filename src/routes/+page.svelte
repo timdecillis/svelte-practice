c3d4ef96dd24d4b6cb45a3562ff5b6dea778e954
Tim DeCillis <timdecillis@gmail.com>
2024-01-08 19:54:28 -0800

    added another form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,20 +5,27 @@
 
   let currentUser: string;
 
-  let flavor: string = "Cookies and Cream";
+  let currentFlavor: string;
 
   const unsubscribe = count.subscribe(({ user, flavor }) => {
+    console.log('flavor:', flavor)
     currentUser = user;
-    flavor = flavor;
+    currentFlavor = flavor;
   });
 
   onDestroy(unsubscribe);
 </script>
 
-<Flavor user={currentUser} />
+<Flavor flavor={currentFlavor} user={currentUser} />
 
-<form method="post" on:submit|preventDefault>
+<form method="post" action="user" on:submit|preventDefault>
   <label for="username">change username</label>
   <input id="username" type="text"/>
+  <input type="submit" value="Save"/>
+</form>
+
+<form method="post" action="flavor" on:submit|preventDefault>
+  <label for="username">update flavor</label>
+  <input id="flavor" type="text"/>
   <input type="submit" value="Save"/>
 </form>
